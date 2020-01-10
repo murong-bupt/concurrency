feabc98dfb3d30471fe718d0604bd7ebc845dad8
murong <duoyi_xie@163.com>
2020-01-10 11:07:32 +0800

    增加了四个注解

---
+++ src/main/java/com/murong/concurrency/annoations/NotRecommend.java
@@ -0,0 +1,18 @@
+package com.murong.concurrency.annoations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * @author xdy
+ * 用来标记【不推荐】的类或写法
+ */
+//TODO 研究一下两个注解类
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.SOURCE)
+public @interface NotRecommend {
+
+    String value() default "";
+}
---
+++ src/main/java/com/murong/concurrency/annoations/NotThreadSafe.java
@@ -0,0 +1,18 @@
+package com.murong.concurrency.annoations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * @author xdy
+ * 用来标记【线程不安全】的类或写法
+ */
+//TODO 研究一下两个注解类
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.SOURCE)
+public @interface NotThreadSafe {
+
+    String value() default "";
+}
